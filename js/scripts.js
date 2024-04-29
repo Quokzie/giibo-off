efe949f656d5b2a5b1e8d37b85a31c9fcaa6252f
Quokzie <153464931+Quokzie@users.noreply.github.com>
2024-04-28 21:06:50 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
+/*this is the js code for the toast in Publicar.html*/
 const toastTrigger = document.getElementById('liveToastBtn')
 const toastLiveExample = document.getElementById('liveToast')
 
