d1583c23deaa166c5262afaf6015ec232aaddf1c
Quokzie <153464931+Quokzie@users.noreply.github.com>
2024-05-17 11:56:50 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -39,3 +39,15 @@ const offcanvasInstance = new bootstrap.Offcanvas(myOffcanvas);
 
 // Show the offcanvas programmatically:
 offcanvasInstance.show();
+
+
+document.getElementById("my-file").onchange = function() {
+  if (this.files && this.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      // e.target.result is a base64-encoded url that contains the image data
+      document.getElementById('profile-pic').setAttribute('src', e.target.result);
+    };
+    reader.readAsDataURL(this.files[0]);
+  }
+}
